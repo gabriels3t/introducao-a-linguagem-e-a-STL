de7c05b7b093ac9f0bffa162f31aac0fcf38a630
gabriels3t <gabriels3t@hotmail.com>
2021-06-04 19:34:10 -0300

    colocando pontuação

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -9,12 +9,15 @@ int main (){
     bool acertou_numero_secreto =false;
     int chute;
     int tentativas = 0;
-    
+    float pontos = 1000.0f;
+    float pontos_perdidos = 0.0f;
     do{
         tentativas++;
         cout << "Tentativa numero : " << tentativas << endl;
         cout << "Qual seu chute ? " << endl;
         cin >>  chute;
+        pontos_perdidos = abs(chute - NUMERO_SECRETO)/2.0;
+        pontos -= pontos_perdidos;
         cout << "O chute foi : " << chute << endl;
         bool acertou = chute == NUMERO_SECRETO;
         bool chute_maior = chute > NUMERO_SECRETO;
@@ -27,7 +30,10 @@ int main (){
             cout << "Seu chute foi menor que o numero secreto" << endl;
         }
     }while(!acertou_numero_secreto);
-    cout << "O numero de tentativas foi : " << tentativas <<endl;
     cout << "Fim de jogo " << endl;
+    cout << "O numero de tentativas foi : " << tentativas <<endl;
+    cout.precision(2); // Passando uma precissao de 2 casas
+    cout << fixed; // deixando fixo para nao ficar em notação cientifica
+    cout << "Sua pontuacao foi de : " << pontos << endl;
     
 }
