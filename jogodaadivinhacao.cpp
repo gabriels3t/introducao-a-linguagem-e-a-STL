c2d74bb0dd8862911da201f8154fa211bd122a1e
gabriels3t <gabriels3t@hotmail.com>
2021-06-04 15:46:40 -0300

    Verificando tentativas

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -5,7 +5,29 @@ int main (){
     cout << "* Boas vindas ao Jogo da Adivinhacaoo! *"<< endl;
     cout << "*************************************** "<< endl;
      
-    int numero_secreto = 42;
-    cout << "O numero secreto e. " << numero_secreto <<". nao conte pra ninguem"<< endl;        
-
+    const int NUMERO_SECRETO = 42;
+    bool acertou_numero_secreto =false;
+    int chute;
+    int tentativas = 0;
+    
+    do{
+        tentativas++;
+        cout << "Tentativa numero : " << tentativas << endl;
+        cout << "Qual seu chute ? " << endl;
+        cin >>  chute;
+        cout << "O chute foi : " << chute << endl;
+        bool acertou = chute == NUMERO_SECRETO;
+        bool chute_maior = chute > NUMERO_SECRETO;
+        if(acertou){
+            cout << "Parabens voce acertou !!"<< endl;
+            acertou_numero_secreto = true;
+        }else if(chute_maior){
+            cout << "Seu chute foi maior que o numero secreto" << endl;
+        }else{
+            cout << "Seu chute foi menor que o numero secreto" << endl;
+        }
+    }while(!acertou_numero_secreto);
+    cout << "O numero de tentativas foi : " << tentativas <<endl;
+    cout << "Fim de jogo " << endl;
+    
 }
